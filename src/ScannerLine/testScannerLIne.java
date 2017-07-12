1c878a3f4e81e00c33aef39da8439255d949c56b
PatrickNg <PatrickNgZh@outlook.com>
2017-07-12 21:00:37 +0800

    add ScannerLine

---
+++ src/ScannerLine/testScannerLIne.java
@@ -0,0 +1,15 @@
+package ScannerLine;
+
+import java.util.Scanner;
+
+public class testScannerLIne {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		while(s.hasNext()) {
+			String src=s.nextLine();
+			System.out.println(src);
+		}
+	}
+
+}
