d2b6d9f6e3c085d2cc4aec78af196d1461d3a2be
PatrickNg <PatrickNgZh@outlook.com>
2017-07-12 23:13:50 +0800

    add the Reverse

---
+++ src/ReverseAndForeach/testCollectionsReverse.java
@@ -0,0 +1,25 @@
+package ReverseAndForeach;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class testCollectionsReverse {
+
+	public static void main(String[] args) {
+		ArrayList<String> tar=new ArrayList<String>();
+		tar.add("hello ");
+		tar.add("my ");
+		tar.add("name ");
+		tar.add("is ");
+		tar.add("cong ");
+		for (String string : tar) {
+			System.out.print(string);
+		}
+		System.out.println();
+		Collections.reverse(tar);
+		for (String string : tar) {
+			System.out.print(string);
+		}
+	}
+
+}
