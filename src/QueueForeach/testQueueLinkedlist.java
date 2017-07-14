1edda0c96fa4eeab1fc142cd2be370db454a23b5
PatrickNg <PatrickNgZh@outlook.com>
2017-07-14 20:18:41 +0800

    fix the comment of QueueForeach

--- src/QueueForeach/testQueueLinkedlist.java
+++ src/QueueForeach/testQueueLinkedlist.java
@@ -5,9 +5,9 @@ import java.util.Queue;
 
 public class testQueueLinkedlist {
 	public static void main(String[] args) {
-		Queue<Integer> test=new LinkedList<Integer>();
-		for (int i = 0; i < 8; i++) {
-			test.add(i);
+		Queue<Integer> test=new LinkedList<Integer>();//fix the infornation of OOP
+		for (int i = 0; i < 8; i++) {//the type of Queue is the t
+			test.add(i);             //the realization of type is LinkedList
 		}
 		for (Integer integer : test) {
 			System.out.print(integer+" ");
