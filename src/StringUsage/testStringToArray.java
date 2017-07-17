f8a424d5daf34a75028a1af506d2699214116909
PatrickNg <PatrickNgZh@outlook.com>
2017-07-17 09:53:10 +0800

    add the StringToArray

---
+++ src/StringUsage/testStringToArray.java
@@ -0,0 +1,14 @@
+package StringUsage;
+
+public class testStringToArray {
+
+	public static void main(String[] args) {
+		String src="12345";
+		char[] array=src.toCharArray();
+		System.out.println(array);
+		String temp;
+		temp=String.valueOf(array);
+
+	}
+
+}
